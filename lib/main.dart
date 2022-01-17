499e20bc7ee3d36abe1e758ba6f53249e6c00576
unknown <mehdibsmii@gmail.com>
2022-01-17 06:58:29 -0800

    add tabBar1 Body

--- lib/main.dart
+++ lib/main.dart
@@ -128,10 +128,160 @@ class _MyHomePageState extends State<MyHomePage>
           ),
         ),
         body: SafeArea(
-          child: TabBarView(
-              controller: _tabController,
-              children: [Container(), Container(), Container()]),
+          child: TabBarView(controller: _tabController, children: [
+            SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Getting\nStarted',
+                      style: TextStyle(fontSize: 36),
+                    ),
+                    const SizedBox(
+                      height: 35,
+                    ),
+                    Text(
+                      'Personal details',
+                      style: TextStyle(
+                          fontSize: 18, color: Theme.of(context).primaryColor),
+                    ),
+                    const SizedBox(
+                      height: 13,
+                    ),
+                    const CustomCard(
+                      title: 'System related questions:',
+                      icon: Icons.arrow_drop_up_sharp,
+                      caption:
+                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus phasellus sollicitudin pretium, est id massa turpis accumsan ',
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    const CustomCard(
+                      title: 'Support related questions:',
+                      icon: Icons.arrow_drop_down_sharp,
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    const CustomCard(
+                      title: 'How do I add new credit card',
+                      icon: Icons.arrow_drop_down_sharp,
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    const Text(
+                      'See all 12 articles',
+                      style: TextStyle(fontSize: 14, color: Color(0xFF004225)),
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    Text(
+                      'Discover',
+                      style: TextStyle(
+                          fontSize: 18, color: Theme.of(context).primaryColor),
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    const CustomCard(
+                      title: 'System related questions:',
+                      caption:
+                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus phasellus sollicitudin pretium, est id massa turpis accumsan ',
+                      icon: Icons.arrow_drop_up_sharp,
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    const CustomCard(
+                      title: 'Support related questions:',
+                      icon: Icons.arrow_drop_down_sharp,
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    const CustomCard(
+                      title: 'How do I add new credit card',
+                      icon: Icons.arrow_drop_down_sharp,
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Container(
+                      height: 64.0,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      child: Material(
+                        color: Colors.transparent,
+                        child: InkWell(
+                          onTap: () {},
+                          borderRadius: BorderRadius.circular(16),
+                          child: Container(
+                            alignment: Alignment.center,
+                            child: const Text(
+                              'Contact Us',
+                              style: TextStyle(
+                                  fontSize: 22, color: Color(0xFFEED448)),
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Container(),
+            Container()
+          ]),
         ),
+      ),
+    );
+  }
+}
+
+class CustomCard extends StatelessWidget {
+  const CustomCard({
+    required this.title,
+    required this.icon,
+    this.caption = '',
+    Key? key,
+  }) : super(key: key);
+  final String title;
+  final IconData icon;
+  final String caption;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+          color: Colors.grey[100],
+          borderRadius: BorderRadius.circular(8),
+          border: Border.all(color: Colors.grey.withOpacity(0.3), width: 1)),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Text(
+                title,
+                style: const TextStyle(fontSize: 16),
+              ),
+              const Spacer(),
+              Icon(icon)
+            ],
+          ),
+          if (caption.isNotEmpty)
+            Text(caption,
+                style: TextStyle(
+                    fontSize: 14, color: Colors.black.withOpacity(0.7)))
+        ],
       ),
     );
   }
